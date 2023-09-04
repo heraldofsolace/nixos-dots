da82efdd37c81806e141c3ba97611addad96c124
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2023-09-05 00:17:04 +0530

    fix Miranda

--- cells/nixos/hosts/miranda/_hardware-configuration.nix
+++ cells/nixos/hosts/miranda/_hardware-configuration.nix
@@ -9,24 +9,24 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-amd"];
+  boot.extraModulePackages = [];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/9230ab9d-eca3-4b71-93d5-0a3c41d47bc7";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/9230ab9d-eca3-4b71-93d5-0a3c41d47bc7";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/38F1-3EA0";
-      fsType = "vfat";
-    };
+  fileSystems."/boot/efi" = {
+    device = "/dev/disk/by-uuid/D074-7B2E";
+    fsType = "vfat";
+  };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/27183b98-984f-452b-8ec8-954cc7f26ed6"; }
-    ];
+  swapDevices = [
+    {device = "/dev/disk/by-uuid/27183b98-984f-452b-8ec8-954cc7f26ed6";}
+  ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
